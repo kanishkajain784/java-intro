03b1454d2be8b9a18a712a5984c5ac256cf5af5b
kanishka jain <kanishkjain784@gmail.com>
2026-03-16 23:32:41 +0530

    Add files via upload

---
+++ diffvar2.java
@@ -0,0 +1,13 @@
+// Import the ArrayList class
+import java.util.ArrayList;
+
+public class diffvar2 {
+  public static void main(String[] args) {
+    var cars = new ArrayList<String>();
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
