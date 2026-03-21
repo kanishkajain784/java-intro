fb7cb4e28fb5fa6eac256ebddaa2ac6c655dc5bd
kanishka jain <kanishkjain784@gmail.com>
2026-03-21 23:11:34 +0530

    Add files via upload

---
+++ charNum.java
@@ -0,0 +1,9 @@
+public class charNum {
+    
+public static void main(String[] args) {
+    char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+    System.out.println(myVar1);
+    System.out.println(myVar2);
+    System.out.println(myVar3);
+  }
+}
